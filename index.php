87055cafe0a4d8238cf46685edba5d93e4bc4293
ng47845 <ng47845@ubt-uni.net>
2024-01-24 22:00:03 +0100

    testing changed path

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <?php
+    echo "Hello world!"
+    echo "<br>";
+    $username = "norag";
+    echo $username;
+    echo "<br>";
+    $current_user = $username;
+    echo $current_user;
+
+    echo "<br>";
+
+    $count = 17;
+    echo $count;
+
+    echo "<br>";
+
+    
+
+?>
+
+
+</body>
+</html>
+
+
+
